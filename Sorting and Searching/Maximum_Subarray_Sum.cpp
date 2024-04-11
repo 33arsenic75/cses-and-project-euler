4a11501cf92be16bea60b123ec0a9ca46889a8a0
33Arsenic75 <122697564+33Arsenic75@users.noreply.github.com>
2024-04-11 23:46:16 +0530

    Concert_Tickets Maximum_Subarray_Sum Movie_Festival

---
+++ Sorting and Searching/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdlib.h>
+#include <unordered_set>
+#include <vector>
+/// ordered sets support upperbound lowerbound too
+#include <map>
+#include <unordered_map>
+#define len(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+#define int long long
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define f first
+#define mk make_pair
+#define in insert
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define show1(v)                                                               \
+  for (auto i : v)                                                             \
+  cout << i << ' '
+#define show(v)                                                                \
+  show1(v);                                                                    \
+  cout << '\n'
+template <int MOD=998244353>
+struct Modular {
+  int value;
+  static const int MOD_value = MOD;
+
+  Modular(long long v = 0) { value = v % MOD; if (value < 0) value += MOD;}
+  Modular(long long a, long long b) : value(0){ *this += a; *this /= b;}
+
+  Modular& operator+=(Modular const& b) {value += b.value; if (value >= MOD) value -= MOD; return *this;}
+  Modular& operator-=(Modular const& b) {value -= b.value; if (value < 0) value += MOD;return *this;}
+  Modular& operator*=(Modular const& b) {value = (long long)value * b.value % MOD;return *this;}
+
+  friend Modular mexp(Modular a, long long e) {
+    Modular res = 1; while (e) { if (e&1) res *= a; a *= a; e >>= 1; }
+    return res;
+  }
+  friend Modular inverse(Modular a) { return mexp(a, MOD - 2); }
+
+  Modular& operator/=(Modular const& b) { return *this *= inverse(b); }
+  friend Modular operator+(Modular a, Modular const b) { return a += b; }
+  friend Modular operator-(Modular a, Modular const b) { return a -= b; }
+  friend Modular operator-(Modular const a) { return 0 - a; }
+  friend Modular operator*(Modular a, Modular const b) { return a *= b; }
+  friend Modular operator/(Modular a, Modular const b) { return a /= b; }
+  friend std::ostream& operator<<(std::ostream& os, Modular const& a) {return os << a.value;}
+  friend bool operator==(Modular const& a, Modular const& b) {return a.value == b.value;}
+  friend bool operator!=(Modular const& a, Modular const& b) {return a.value != b.value;}
+};
+template <typename T> void chkmin(T &x, T y) {
+  if (x > y)
+    x = y;
+}
+template <typename T> void chkmax(T &x, T y) {
+  if (x < y)
+    x = y;
+}
+#ifndef ONLINE_JUDGE
+#define debug(x) cout << #x << " = " << x << endl
+#define __gcd __algo_gcd
+#else
+#define debug(x)
+#endif
+
+
+// ------------------------------------------***--------------------------------------------------
+
+
+
+
+int32_t main() {
+    int n;cin>>n;
+    vector<int>a(n);
+    rep(i,0,n)cin>>a[i];
+    vector<int>ans(n,0);
+    ans[0]=a[0];
+    rep(i,1,n){
+        ans[i] = max(a[i],a[i]+ans[i-1]);
+    }
+    cout<<*max_element(all(ans));
+  return 0;
+}
