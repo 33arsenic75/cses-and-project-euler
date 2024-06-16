9f8e5055e9f6f86ce8fd8413a31bef3db61241a0
33Arsenic75 <122697564+33Arsenic75@users.noreply.github.com>
2024-06-16 21:23:32 +0530

    .

---
+++ Dynamic Programming/Two_Sets_II.cpp
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdlib.h>
+#include <unordered_set>
+#include <vector>
+#include<functional>
+/// ordered sets support upperbound lowerbound too
+#include <map>
+#include <unordered_map>
+#define len(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+#define int long long
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define f first
+#define mk make_pair
+#define in insert
+#define py cout << "YES\n"
+#define pn cout << "NO\n"
+#define fill(v,a,b) rep(i,a,b)cin>>v[i]
+template <typename T> void chkmin(T &x, T y) {
+  if (x > y)
+    x = y;
+}
+template <typename T> void chkmax(T &x, T y) {
+  if (x < y)
+    x = y;
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cout << #x << " = " << x << endl
+#define show1(v)                                                               \
+  for (auto i : v)                                                             \
+  cout << i << ' '
+#define show(v)                                                                \
+  show1(v);                                                                    \
+  cout << '\n'
+#define __gcd __algo_gcd
+#else
+#define debug(x)
+#endif
+const int INF = 1e9;
+const int LINF = INF * INF;
+
+// ------------------------------------------***--------------------------------------------------
+
+
+
+
+
+template <int MOD=1000'000'007>
+struct Modular {
+  int value;
+  static const int MOD_value = MOD;
+
+  Modular(long long v = 0) { value = v % MOD; if (value < 0) value += MOD;}
+  Modular(long long a, long long b) : value(0){ *this += a; *this /= b;}
+
+  Modular& operator+=(Modular const& b) {value += b.value; if (value >= MOD) value -= MOD; return *this;}
+  Modular& operator-=(Modular const& b) {value -= b.value; if (value < 0) value += MOD;return *this;}
+  Modular& operator*=(Modular const& b) {value = (long long)value * b.value % MOD;return *this;}
+
+  friend Modular mexp(Modular a, long long e) {
+    Modular res = 1; while (e) { if (e&1) res *= a; a *= a; e >>= 1; }
+    return res;
+  }
+  friend Modular inverse(Modular a) { return mexp(a, MOD - 2); }
+
+  Modular& operator/=(Modular const& b) { return *this *= inverse(b); }
+  friend Modular operator+(Modular a, Modular const b) { return a += b; }
+  friend Modular operator-(Modular a, Modular const b) { return a -= b; }
+  friend Modular operator-(Modular const a) { return 0 - a; }
+  friend Modular operator*(Modular a, Modular const b) { return a *= b; }
+  friend Modular operator/(Modular a, Modular const b) { return a /= b; }
+  friend std::ostream& operator<<(std::ostream& os, Modular const& a) {return os << a.value;}
+  friend bool operator==(Modular const& a, Modular const& b) {return a.value == b.value;}
+  friend bool operator!=(Modular const& a, Modular const& b) {return a.value != b.value;}
+};
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;cin>>n;
+    int target = (n*(n+1))/2;
+    if(target%2==0)target/=2;
+    else{
+        cout<<0<<'\n';
+        return 0;
+    }
+    vector<vector<Modular<>>>dp(n+1,vector<Modular<>>(target+1,0));
+    dp[0][0] = 1;
+    for(int i = 1; i <=n ; i++){
+        for(int j = 0 ; j <= target ; j++){
+            if(j>=i)dp[i][j] = (dp[i-1][j] + dp[i-1][j-i]);
+            else dp[i][j] = dp[i-1][j];
+        }
+    }
+    cout<<dp[n][target]/2;
+    return 0;
+}
