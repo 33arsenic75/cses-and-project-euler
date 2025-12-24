4a17fe2ee8774720df0a9bce04d806a7a825b8f7
Abhinav Rajesh Shripad <abhinav@Abhinavs-MacBook-Pro-3.local>
2025-12-24 21:58:01 +0530

    2025-12-24 21:58:01

---
+++ Next_Prime.cpp
@@ -0,0 +1,110 @@
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdlib.h>
+#include <unordered_set>
+#include <vector>
+#include<functional>
+#include<cmath>
+#include<numeric>
+/// ordered sets support upperbound lowerbound too
+#include <map>
+#include <unordered_map>
+#include <cstring>
+#define len(v) (ll)v.size()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define pb push_back
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define f first
+#define mk make_pair
+#define in insert
+#define py cout << "YES\n"
+#define pn cout << "NO\n"
+#define fill(v,a,b) rep(i,a,b)cin>>v[i]
+template <typename T> void chkmin(T &x, T y) {
+  if (x > y)
+    x = y;
+}
+template <typename T> void chkmax(T &x, T y) {
+  if (x < y)
+    x = y;
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cout << #x << " = " << x << endl
+#define show1(v)                                                               \
+  for (auto i : v)                                                             \
+  cout << i << ' '
+#define show(v)                                                                \
+  show1(v);                                                                    \
+  cout << '\n'
+#else
+#define debug(x)
+#endif
+const ll INF = 1e9;
+const ll LINF = INF * INF;
+
+// ------------------------------------------***--------------------------------------------------
+
+ll gcd(ll a, ll b){
+    if(b==0)return a;
+    return gcd(b,a%b);
+}
+
+vector<ll> sieve(ll n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+
+    for (ll i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (ll j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    vector<ll> primes;
+    for (ll i = 2; i <= n; i++) {
+        if (is_prime[i]) primes.push_back(i);
+    }
+
+    return primes;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll M = 1e6 + 5;
+    vector<ll>prm = sieve(M);
+    ll t,x;
+    cin>>t;
+    function<bool(ll)>is_prime = [&](ll n){
+        for(ll p: prm){
+            if(p==n)return true;
+            else if(n%p==0)return false;
+            else if(p>n)return true;
+        }
+        return true;
+    };
+    while(t--){
+        cin>>x;
+        x++;
+        while(!is_prime(x))x++;
+        cout<<x<<'\n';
+    }
+    return 0;
+}
+
+
+
+
+
+
