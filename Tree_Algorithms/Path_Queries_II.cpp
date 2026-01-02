bbd93cc26c275d9f1e1e8020f0bc5b3255731a3d
Abhinav Rajesh Shripad <abhinav@Abhinavs-MacBook-Pro-3.local>
2026-01-02 11:34:10 +0530

    2026-01-02 11:34:10

--- Tree_Algorithms/Path_Queries_II.cpp
+++ Tree_Algorithms/Path_Queries_II.cpp
@@ -1,3 +1,9 @@
+// Problem: Path_Queries_II
+// File: Path_Queries_II.cpp
+// Created: 2026-01-02
+// Author: abhinav
+//
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -21,7 +27,7 @@ const ll LINF = INF * INF;
 
 template <typename T> class Segmenttree {
 public:
-  const int sz;
+  int sz;
   vector<T> tree;
   vector<T> lazy;
   vector<T> arr;
@@ -102,6 +108,9 @@ private:
   }
 
 public:
+  Segmenttree() {
+
+  };
   Segmenttree(int n) : sz(n), tree(4 * n, ID), lazy(4 * n, LAZY_ID) {}
 
   Segmenttree(const vector<T> &a)
@@ -118,20 +127,106 @@ public:
   T get(ll pos) { return range_sum(1, 0, sz - 1, pos, pos); }
 };
 
-ll gcd(ll a, ll b) {
-  if (b == 0)
-    return a;
-  return gcd(b, a % b);
-}
+template <class T, bool VALS_IN_EDGES> class HLD {
+private:
+  ll N, R, tim = 0; // n, root node, time
+  vector<vector<ll>> adj;
+  vector<ll> par, siz, depth, rt,
+      pos;                // parent, size, depth, root, position arrays
+  Segmenttree<T> segtree; // Modify as needed
 
-int32_t main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  void dfs_sz(ll v) {
+    if (par[v] != -1)
+      adj[v].erase(find(adj[v].begin(), adj[v].end(), par[v]));
+    for (ll &u : adj[v]) {
+      par[u] = v, depth[u] = depth[v] + 1;
+      dfs_sz(u);
+      siz[v] += siz[u];
+      if (siz[u] > siz[adj[v][0]])
+        swap(u, adj[v][0]);
+    }
+  }
+
+  void dfs_hld(ll v) {
+    pos[v] = tim++;
+    for (ll u : adj[v]) {
+      rt[u] = (u == adj[v][0] ? rt[v] : u);
+      dfs_hld(u);
+    }
+  }
+
+  template <class B> void process(ll u, ll v, B op) {
+    for (; rt[u] != rt[v]; v = par[rt[v]]) {
+      if (depth[rt[u]] > depth[rt[v]])
+        swap(u, v);
+      op(pos[rt[v]], pos[v]);
+    }
+    if (depth[u] > depth[v])
+      swap(u, v);
+    op(pos[u] + VALS_IN_EDGES, pos[v]);
+  }
+
+public:
+  HLD(vector<vector<ll>> adj_, ll _Root, const vector<T> &base_)
+      : N(adj_.size()), R(_Root), adj(adj_), par(N, -1), siz(N, 1), depth(N),
+        rt(N), pos(N) // modify if need
+  {
+    rt[R] = _Root;
+    dfs_sz(R);
+    dfs_hld(R);
+    vector<ll> temp(N, 0);
+    // for (ll i = 0; i < N; i++)
+    //   temp[pos[i]] = base_[i];
+    // segtree = Segmenttree<ll>(temp);
+  }
+
+  T query_path(ll u, ll v) {
+    T res = 0; // default value, modify depending on problem
+    process(u, v, [&](ll l, ll r) {
+      res = max(res, segtree.range_query(l, r)); // modify depending on problem
+    });
+    return res;
+  }
+  void setsegtree(const vector<T> &base) {
+    vector<T> temp(base.size());
+    for (ll i = 0; i < base.size(); i++)
+      temp[pos[i]] = base[i];
+    segtree = Segmenttree<T>(temp);
+  }
+  void modify_node(ll u, T val) { segtree.range_update(pos[u], pos[u], val); }
+
+  void modify_subtree(ll v, T val) {
+    // segtree.range_update(pos[v] + VALS_IN_EDGES, pos[v] + siz[v] - 1,
+    //                      Query{ADD, val}); // modify if need
+    segtree.range_update(pos[v] + VALS_IN_EDGES, pos[v] + siz[v] - 1, val);
+  }
+};
+
+int main() {
+  ll n, q;
+  cin >> n >> q;
+  vector<ll> base(n + 1);
+  rep(i, 1, n + 1) cin >> base[i];
+  vector<vector<ll>> adj(n + 1);
+  ll u, v;
+  rep(i, 0, n - 1) {
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  HLD<ll, false> hld(adj, 1, base);
+  // rep(i, 1, n + 1) hld.modify_node(i, base[i]);
+  hld.setsegtree(base);
+  ll type, s, x;
+  while (q--) {
+    cin >> type;
+    if (type == 1) {
+      cin >> s >> x;
+      hld.modify_node(s, x);
+    } else {
+      cin >> u >> v;
+      cout << hld.query_path(u, v) << " ";
+    }
+  }
   return 0;
 }
-
-// 27
-// 11
-// 45
-// 9
-// 34
