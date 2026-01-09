81bb75c4bbe93f9d869c4f1407c3388bf8744cf9
Abhinav Rajesh Shripad <abhinav@Abhinavs-MacBook-Pro-3.local>
2026-01-09 19:09:27 +0530

    2026-01-09 19:09:27

---
+++ String_Algorithms/Finding_Borders.cpp
@@ -0,0 +1,64 @@
+// Problem: Finding Borders
+// File: Finding_Borders.cpp
+// Created: 2026-01-09
+// Author: abhinav
+// Link:
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define py cout << "YES\n"
+#define pn cout << "NO\n"
+#define debug(x) cout << #x << " = " << x << endl
+#define show1(v)                                                               \
+  for (auto i : v)                                                             \
+  cout << i << ' '
+#define show(v)                                                                \
+  show1(v);                                                                    \
+  cout << '\n'
+const ll INF = 1e9;
+const ll LINF = INF * INF;
+
+template <typename T> auto make_vec(size_t n, T val) {
+  return std::vector<T>(n, val);
+}
+
+template <typename T, typename... Args> auto make_vec(size_t n, Args... args) {
+  return std::vector<decltype(make_vec<T>(args...))>(n, make_vec<T>(args...));
+}
+
+vector<ll> z_value(string &s) {
+  ll n = s.size();
+  vector<ll> z(n, 0);
+  ll l = 0, r = 0;
+  rep(i, 1, n) {
+    if (i < r) {
+      z[i] = min(r - i, z[i - l]);
+    }
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      z[i]++;
+    }
+    if (i + z[i] > r) {
+      l = i;
+      r = i + z[i];
+    }
+  }
+  return z;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  string s;
+  cin >> s;
+  ll n = s.size();
+  vector<ll> z = z_value(s);
+  for (ll i = n - 1; i > 0; i--) {
+    if (z[i] == n - i)
+      cout << z[i] << " ";
+  }
+  return 0;
+}
